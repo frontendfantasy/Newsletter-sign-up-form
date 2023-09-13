24ec7947533c4c928a1cb3bc018c2a04838264dc
frontendfantasy <141944140+frontendfantasy@users.noreply.github.com>
2023-09-13 17:53:09 +0500

    Initial commit

---
+++ assets/JS/index.js
@@ -0,0 +1,9 @@
+var btn = document.querySelector('.btn');
+var card = document.querySelector('.card');
+var successCard = document.querySelector('.success-card');
+
+
+btn.addEventListener ('click', () => {
+        card.classList.add('visibility-hidden')
+  
+})
