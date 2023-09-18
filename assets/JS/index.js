7b4a7d9af3acba0df4d7bc3a41633b9d6b798dc1
frontendfantasy <141944140+frontendfantasy@users.noreply.github.com>
2023-09-18 20:59:38 +0500

    get button working

--- assets/JS/index.js
+++ assets/JS/index.js
@@ -1,9 +1,20 @@
 var btn = document.querySelector('.btn');
+// var btn = document.querySelectorAll('.btn');
 var card = document.querySelector('.card');
 var successCard = document.querySelector('.success-card');
 
 
-btn.addEventListener ('click', () => {
-        card.classList.add('visibility-hidden')
-  
-})
+// btn.array.forEach(element => {
+//         addEventListener('click', function() {
+
+btn.addEventListener('click', function() {
+
+   if(successCard.classList.contains('visibility-hidden')) {
+        successCard.classList.remove('visibility-hidden');
+        card.classList.add('visibility-hidden');
+}
+else {
+        successCard.classList.add('visibility-hidden');
+        card.classList.remove('visibility-hidden');
+        }
+   });
